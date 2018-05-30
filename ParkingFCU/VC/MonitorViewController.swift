12c2932d8c24ebac83cc2494442a78f7c878018d
Unknown <ken51006b@hotmail.com>
2018-05-30 19:34:02 +0800

    新增Charts

--- ParkingFCU/VC/MonitorViewController.swift
+++ ParkingFCU/VC/MonitorViewController.swift
@@ -7,14 +7,143 @@
 //
 
 import UIKit
+import Charts
 
-class MonitorViewController: UIViewController {
-
+class MonitorViewController: UIViewController , ChartViewDelegate{
+    
+        
+    @IBOutlet weak var chartView: LineChartView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+//        self.options = [.toggleValues,
+//                        .toggleFilled,
+//                        .toggleCircles,
+//                        .toggleCubic,
+//                        .toggleHorizontalCubic,
+//                        .toggleStepped,
+//                        .toggleHighlight,
+//                        .animateX,
+//                        .animateY,
+//                        .animateXY,
+//                        .saveToGallery,
+//                        .togglePinchZoom,
+//                        .toggleAutoScaleMinMax,
+//                        .toggleData]
+//
+        chartView.delegate = self
+        
+        chartView.chartDescription?.enabled = false
+        chartView.dragEnabled = true
+        chartView.setScaleEnabled(true)
+        chartView.pinchZoomEnabled = true
+        
+        let l = chartView.legend
+        l.form = .line
+        l.font = UIFont(name: "HelveticaNeue-Light", size: 11)!
+        l.textColor = .black
+        l.horizontalAlignment = .left
+        l.verticalAlignment = .bottom
+        l.orientation = .horizontal
+        l.drawInside = false
+        
+        let xAxis = chartView.xAxis
+        xAxis.labelFont = .systemFont(ofSize: 11)
+        xAxis.labelTextColor = .white
+        xAxis.drawAxisLineEnabled = false
+        
+        let leftAxis = chartView.leftAxis
+        leftAxis.labelTextColor = UIColor(red: 51/255, green: 181/255, blue: 229/255, alpha: 1)
+        leftAxis.axisMaximum = 100
+        leftAxis.axisMinimum = 0
+        leftAxis.drawGridLinesEnabled = true
+        leftAxis.granularityEnabled = true
+        
+        let rightAxis = chartView.rightAxis
+        rightAxis.labelTextColor = .red
+        rightAxis.axisMaximum = 100
+        rightAxis.axisMinimum = 0
+        rightAxis.granularityEnabled = false
+        
+//        sliderX.value = 20
+//        sliderY.value = 30
+//        slidersValueChanged(nil)
+        self.setDataCount(Int(15), range: UInt32(50))
+
+        chartView.animate(xAxisDuration: 2.5)
 
         // Do any additional setup after loading the view.
     }
+//    override func updateChartData() {
+//        if self.shouldHideData {
+//            chartView.data = nil
+//            return
+//        }
+//
+//        self.setDataCount(Int(sliderX.value + 1), range: UInt32(sliderY.value))
+//    }
+    
+    func setDataCount(_ count: Int, range: UInt32) {
+        let yVals1 = (0..<count).map { (i) -> ChartDataEntry in
+            let mult = range / 2
+            let val = Double(arc4random_uniform(mult) + 50)
+            return ChartDataEntry(x: Double(i), y: val)
+        }
+        let yVals2 = (0..<count).map { (i) -> ChartDataEntry in
+            let val = Double(arc4random_uniform(range) + 30)
+            return ChartDataEntry(x: Double(i), y: val)
+        }
+        let yVals3 = (0..<count).map { (i) -> ChartDataEntry in
+            let val = Double(arc4random_uniform(range) + 10)
+            return ChartDataEntry(x: Double(i), y: val)
+        }
+        
+        let set1 = LineChartDataSet(values: yVals1, label: "DataSet 1")
+        set1.axisDependency = .left
+        set1.setColor(UIColor(red: 51/255, green: 181/255, blue: 229/255, alpha: 1))
+        set1.setCircleColor(.white)
+        set1.lineWidth = 2
+        set1.circleRadius = 3
+        set1.fillAlpha = 65/255
+        set1.fillColor = UIColor(red: 51/255, green: 181/255, blue: 229/255, alpha: 1)
+        set1.highlightColor = UIColor(red: 244/255, green: 117/255, blue: 117/255, alpha: 1)
+        set1.drawCircleHoleEnabled = false
+        set1.drawFilledEnabled = true
+        set1.mode = .cubicBezier
+        
+        let set2 = LineChartDataSet(values: yVals2, label: "DataSet 2")
+        set2.axisDependency = .right
+        set2.setColor(.red)
+        set2.setCircleColor(.white)
+        set2.lineWidth = 2
+        set2.circleRadius = 3
+        set2.fillAlpha = 65/255
+        set2.fillColor = .red
+        set2.highlightColor = UIColor(red: 244/255, green: 117/255, blue: 117/255, alpha: 1)
+        set2.drawCircleHoleEnabled = false
+        set2.drawFilledEnabled = true
+        set2.mode = .cubicBezier
+
+        let set3 = LineChartDataSet(values: yVals3, label: "DataSet 3")
+        set3.axisDependency = .right
+        set3.setColor(.yellow)
+        set3.setCircleColor(.white)
+        set3.lineWidth = 2
+        set3.circleRadius = 3
+        set3.fillAlpha = 65/255
+        set3.fillColor = UIColor.yellow.withAlphaComponent(200/255)
+        set3.highlightColor = UIColor(red: 244/255, green: 117/255, blue: 117/255, alpha: 1)
+        set3.drawCircleHoleEnabled = false
+        set3.drawFilledEnabled = true
+        set3.mode = .cubicBezier
+
+        let data = LineChartData(dataSets: [set1, set2, set3])
+        data.setValueTextColor(.black)
+        data.setValueFont(.systemFont(ofSize: 6))
+        
+        chartView.data = data
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
